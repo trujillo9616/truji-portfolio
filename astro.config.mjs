c44d3573c2e8e0d99d6d1492b68cecf7f64a84f3
Adrian Trujillo <adrian.td96@gmail.com>
2023-04-23 17:11:35 -0600

    feat: :sparkles: added astro integrations
    Added react, tailwind, image, prefetch and sitemap integrations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), prefetch(), sitemap(), image()]
+});
